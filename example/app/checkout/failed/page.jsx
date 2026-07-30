2ef56af4f2417220b4b9dd7f8f54b23ad58cf7af
Masrur Ahmmed <mruurx@gmail.com>
2026-07-30 10:27:12 +0600

    Add files via upload

--- example/app/checkout/failed/page.jsx
+++ example/app/checkout/failed/page.jsx
@@ -1,1 +1,8 @@
-
+export default function Failed() {
+  return (
+    <div style={{ textAlign: 'center', padding: 64, fontFamily: 'sans-serif' }}>
+      <h1>We couldn't confirm that payment</h1>
+      <p>No matching transaction was found in time. You can try the checkout again.</p>
+    </div>
+  );
+}
